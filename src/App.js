ba22af1ac43dd945ffd793d3015f8ee5157ed1f3
YoshiYodaFoster <noahbfoster@gmail.com>
2020-06-18 17:18:29 -0400

    Fixed Style Sheets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'bulma/css/bulma.css';
+import {
+  Container,
+  Columns,
+  Footer,
+  Content,
+  Column,
+  Icon
+} from "bloomer";
+import Menubar from './components/Menubar';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+
+  }
+  render() {
+    return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+          <Menubar/>
+          <Route 
+            path="/"
+            exact strict
+            render={props => (
+              <div><Container>
+                
+              </Container>
+              <br></br></div>
+            )}
+          />
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Menubar.js
@@ -0,0 +1,53 @@
+import React from "react";
+//import ReactDOM from 'react-dom';
+import {
+  Navbar,
+  NavbarItem,
+  NavbarMenu,
+  NavbarStart,
+  NavbarEnd,
+  HeroFooter,
+  Hero,
+  HeroBody,
+  Container,
+  Title
+} from "bloomer";
+//import './Menubar.css'
+
+export default class Menubar extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+
+    return (
+      <Hero isColor="info" isSize="1/4">
+        <HeroBody>
+          <Container hasTextAlign="left">
+            <Title>Personal Website Testing.</Title>
+          </Container>
+        </HeroBody>
+        <HeroFooter>
+          <Navbar
+            style={{
+              marginLeft: "14%",
+              marginRight: "14%"
+            }}
+          >
+            <NavbarMenu isActive="true">
+              <NavbarStart>
+                <NavbarItem href="search">Search</NavbarItem>
+              </NavbarStart>
+              <NavbarEnd>
+
+              </NavbarEnd>
+            </NavbarMenu>
+          </Navbar>
+        </HeroFooter>
+      </Hero>
+    );
+  }
+
+ 
+}
