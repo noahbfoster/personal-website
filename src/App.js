e6c6ef3623f8c3649338416a86fbc9e6462eaf12
YoshiYodaFoster <noahbfoster@gmail.com>
2020-06-19 16:01:07 -0400

    react-pdf and dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import 'bulma/css/bulma.css';
+import { Document, Page } from 'react-pdf';
 import {
   Container,
   Columns,
@@ -40,8 +41,18 @@ class App extends Component {
 
   }
   
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
   render() {
     const resumeframe = "<iframe src='https://docs.google.com/document/d/e/2PACX-1vT9g8ltEAfkQsUR5FpzqVf-SMxNhNiwjYv8Tdk2gyZ1od3GFDzPVUFx8lIqazqn4w/pub?embedded=true'></iframe>";
+    const { pageNumber, numPages } = this.state;
     return (
       <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
@@ -64,8 +75,14 @@ class App extends Component {
                 <br></br>
                 <div class="doc-container">
                   
-                <iframe src='https://docs.google.com/document/d/e/2PACX-1vT9g8ltEAfkQsUR5FpzqVf-SMxNhNiwjYv8Tdk2gyZ1od3GFDzPVUFx8lIqazqn4w/pub?embedded=true'></iframe>;
-                <embed src='' type="application/pdf" width="100%" height='100%'></embed>
+                
+                <Document
+                  file='resume.pdf'
+                  onLoadSuccess={this.onDocumentLoadSuccess}
+                >
+                  <Page pageNumber={pageNumber} />
+                  <p>Page {pageNumber} of {numPages}</p>
+                </Document>
 
                 </div>
               </Container>
